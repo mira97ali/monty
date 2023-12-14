0f41a163823cc50610d9027eec7314e06553486b
Mira Ali <benamara97amira@gmail.com>
2023-12-14 17:37:52 +0300

    more fixes

--- op_push.c
+++ op_push.c
@@ -1,6 +1,31 @@
 #include "monty.h"
 
 /**
+ * is_number - Check if a string is a valid number.
+ * @str: The string that we need to check.
+ *
+ * Return: 1 if the string is integer, othewise it's 0
+ */
+int is_number(char *str)
+{
+	if (*str == '-')
+		++str;
+
+	if (*str == '\0')
+		return (0);
+
+	while (*str)
+	{
+		if (!isdigit(*str))
+			return (0);
+		str++;
+	}
+
+	return (1);
+}
+
+
+/**
  * op_push - Pushes an element to the stack.
  * @stack: Pointer to the head of the stack.
  * @data: MontyData structure containing necessary information.
@@ -10,7 +35,7 @@ void op_push(stack_t **stack, MontyData *data, unsigned int line_number)
 {
 	char *arg = data->arg;
 
-	if (!arg || !isdigit(*arg))
+	if (!arg || !is_number(*arg))
 	{
 		fprintf(stderr, "L%u: usage: push integer\n", line_number);
 		free_data(*stack, data->fd, data->buffer);
